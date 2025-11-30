8bc479302a1ba3a11c70ca8969b84e134affe1c3
kbj <gauthier.delphine@protonmail.com>
2025-11-30 14:15:22 +0100

    #5 Add Sass & Bem linter
    - Add Sass
    - Implement Bem rules
    - Lint in CI

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,19 @@
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
+import bemLinter from "postcss-bem-linter";
+import postCssReporter from "postcss-reporter";
 
-export default config;
+export default {
+  plugins: [
+    bemLinter({
+      preset: "bem",
+      implicitComponents: "app/**/*.module.scss",
+      // Custom selector to forbid modifiers starting with '_' instead of '--'  // '_' is allowed by BEM, but we chose '--' because it's easier to read
+      componentSelectors: "^.{componentName}(([_]{2}|[-]{2})[a-z-]+)*$", // ignore animations selectors
+      ignoreSelectors: [/^\d+%$/, /^from|to$/],
+    }),
+    postCssReporter({
+      plugins: ["postcss-bem-linter"],
+      clearReportedMessages: true,
+      throwError: true,
+    }),
+  ],
+};
